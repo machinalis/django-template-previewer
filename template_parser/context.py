e2615033ff8e92465b2fce6b7fc9b429e0bb0bea
Daniel F. Moisset <dmoisset@machinalis.com>
2012-03-23 14:15:07 -0300

    Slowly adding support for different kind of nodes

--- template_parser/context.py
+++ template_parser/context.py
@@ -1,11 +1,35 @@
+from collections import namedtuple
+
 from django.template.base import TextNode, VariableNode
+from django.template.defaulttags import (
+    CycleNode, FirstOfNode, LoadNode, NowNode, SpacelessNode, URLNode, WidthRatioNode)
+
+_NodeListWrapper = namedtuple("NodeListWrapper", "nodelist")
+
+
+def _get_vars(filter_expression):
+    """
+    Return the list of vars used in a django filter expressions. That includes
+    the main var and any filter arguments
+    """
+    if not hasattr(filter_expression.var, 'var'):
+        # Probably a string literal or something like that
+        result = []
+    else:
+        result = [filter_expression.var.var]
+    for _, arglist in filter_expression.filters:
+        result.extend(arg.var for lookup, arg in arglist if lookup)
+    return result
+
 
 def get_context(template):
     """
     Returns a list of context variables used in the template. Each variable is
     represented as a dotted string from the context root.
 
-    The argument is a template object, not just a filename
+    The argument is a template object (or anything with a nodelist), not just a filename.
+    Note that many nodes have a "nodelist" attribute, so those are accepted here too
+    (fact which is used in the recursive implementation)
 
     For example, passing the template {{ var }} {{ var.attribute }}
     Results in: ["var", "var.attribute"]
@@ -19,10 +43,47 @@ def get_context(template):
 
     result = []
     for node in template.nodelist:
+        # Ignored nodes
         if isinstance(node, TextNode):
-            pass  # Text is ignored
+            # Text is ignored
+            pass
+        elif isinstance(node, NowNode):
+            pass
+        elif isinstance(node, LoadNode):
+            pass
+        # Simple variables
         elif isinstance(node, VariableNode):
-            result.append(node.filter_expression.var.var)
+            result += _get_vars(node.filter_expression)
+        # Nodes with arguments, non-nested
+        elif isinstance(node, CycleNode):
+            for expr in node.cyclevars:
+                result += _get_vars(expr)
+        elif isinstance(node, FirstOfNode):
+            for expr in node.vars:
+                result += _get_vars(expr)
+        elif isinstance(node, URLNode):
+            if not node.legacy_view_name:  # Django 1.3 new url tag
+                result += _get_vars(node.view_name)
+            for arg in node.args:
+                result += _get_vars(arg)
+            for key, arg in node.kwargs.items():
+                result += _get_vars(arg)
+        elif isinstance(node, WidthRatioNode):
+            result += _get_vars(node.val_expr)
+            result += _get_vars(node.max_expr)
+            result += _get_vars(node.max_width)
+        # Nested nodes
+        elif isinstance(node, SpacelessNode):
+            result += get_context(node)
+        # TODO: block (contents)
+        # TODO: extends (hard, it's not just the argument but the template should be loaded too)
+        # TODO: filter (the arguments, and the content)
+        # TODO: for (hard, the iterable, but also the renamings; then the contents. Also, the forllop stuff)
+        # TODO: if (the expression, and then the contents on each branch
+        # TODO: ifequal, ifnotequal
+        # TODO: include (the argument and the content. also the renamings)
+        # TODO: regroup (the arg, and renamings)
+        # TODO: with: renamings
         else:
-            print node
+            assert False, "Unrecognized node %s" % type(node)
     return result
--- tests/context.py
+++ tests/context.py
@@ -15,5 +15,57 @@ class ContextTest(TestCase):
         vars = context.get_context(t)
         self.assertEqual(vars, ["var1", "var2"])
 
+    def test_chained_vars(self):
+        t = Template("{{ var1.attribute1.inner_attribute2 }}")
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1.attribute1.inner_attribute2"])
+
+    def test_filtered_vars(self):
+        t = Template('{{ var1|default:"Nothing" }}Some text{{ var2|add:"3" }}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var2"])
+
+    def test_filtered_constants(self):
+        t = Template('{{ "constant"|default:var1 }}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1"])
+
+    def test_filter_arguments(self):
+        t = Template('{{ var1|default:var3 }}Some text{{ var2|add:var4 }}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var3", "var2", "var4"])
+
+    def test_simple_tag(self):
+        t = Template('{% cycle var1 "text" var2 %}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var2"])
+
+    def test_block_tag(self):
+        t = Template(
+            '{% spaceless %}This text contains a {{ var }} inside {% endspaceless %}'
+        )
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var"])
+
+    # Tests for specific tags, not covered above
+    def test_firstof(self):
+        t = Template('{% firstof var1 "text" var2 %}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var2"])
+
+    def test_url(self):
+        t = Template('{% url view var1 arg=var2 %}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var2"])
+        # Django 1.5 format:
+        t = Template('{% load url from future %}{% url view var1 arg=var2 %}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["view", "var1", "var2"])
+
+    def test_widthratio(self):
+        t = Template('{% widthratio var1 var2 var3 %}')
+        vars = context.get_context(t)
+        self.assertEqual(vars, ["var1", "var2", "var3"])
+
 if __name__ == '__main__':
     main()
