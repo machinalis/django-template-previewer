952373504009ab3427f00887a4d0eb04adfee49d
Daniel Moisset <dmoisset@machinalis.com>
2012-09-12 12:59:37 -0300

    setup script, license

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+with open('README.md') as readme:
+    __doc__ = readme.read()
+
+setup(
+    name='django-template-previewer',
+    version='0.1',
+    py_modules=['template_previewer'],
+    description=u'A Django app to allow developers preview templates',
+    long_description=__doc__,
+    author=u'Daniel F. Moisset',
+    author_email='dmoisset@machinalis.com',
+    url='https://github.com/machinalis/django-template-previewer',
+    packages=['template_previewer'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
